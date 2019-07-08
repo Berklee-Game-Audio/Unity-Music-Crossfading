389f1f443ec309115fec6c2212f4e843ab6fb4d3
Michael Sweet <msweet37@gmail.com>
2019-07-08 12:06:37 -0400

    Colidation

---
+++ Assets/Standard Assets/Audio/Scripts/TriggerAudio.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class TriggerAudio : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(){
+		AudioSource myAudioSource = GetComponent<AudioSource>();
+        if(myAudioSource == null)
+        {
+            Debug.Log("TriggerAudio: There is no AudioSource attached to this game object.");
+            return;
+        }
+
+        if (myAudioSource.clip == null)
+        {
+            Debug.Log("TriggerAudio: There is no Clip attached to this AudioSource.");
+            return;
+        }
+
+        if (!myAudioSource.isPlaying)
+        {
+            myAudioSource.Stop();
+            myAudioSource.Play();
+        }
+        else
+        {
+            myAudioSource.Play();
+        }
+	}
+}
